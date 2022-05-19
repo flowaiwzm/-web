2d81b1426c89515a3a0574688a8027888e893313
flowaiwzm <wxhwzm@gmail.com>
2022-05-19 10:33:39 +0800

    实验五first commit

---
+++ 实验五/test3.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var a=document.getElementById("demo");
+    var b=document.getElementsByTagName("button")[0];
+    b.innerHTML="Click Me";
+    a.style.display="none";
+ }
---
+++ 实验五/test5.js
@@ -0,0 +1,44 @@
+function fun1(){
+    var a=document.getElementById("1").value;
+    var b=document.getElementById("input2"). value;
+    var c=document.getElementById("output");
+    console.log(a);
+    console.log(b);
+    var n;
+    n=parseInt(a)+parseInt(b);
+    m=n.toFixed(0);
+    c.value=m;
+}
+function fun2(){
+    var a=document.getElementById("1").value;
+    var b=document.getElementById("input2"). value;
+    var c=document.getElementById("output");
+    console.log(a);
+    console.log(b);
+    var n;
+    n=parseInt(a)-parseInt(b);
+    m=n.toFixed(0);
+    c.value=m;
+}
+function fun3(){
+    var a=document.getElementById("1").value;
+    var b=document.getElementById("input2"). value;
+    var c=document.getElementById("output");
+    console.log(a);
+    console.log(b);
+    var n;
+    n=parseInt(a)*parseInt(b);
+    m=n.toFixed(0);
+    c.value=m;
+}
+function fun4(){
+    var a=document.getElementById("1").value;
+    var b=document.getElementById("input2"). value;
+    var c=document.getElementById("output");
+    console.log(a);
+    console.log(b);
+    var n;
+    n=parseInt(a)/parseInt(b);
+    m=n.toFixed(0);
+    c.value=m;
+}
